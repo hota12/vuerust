4a0a8047bfd7bd9abd070174802de839ddc5e191
antonintegrare <integraredesign2@gmail.com>
2022-05-05 19:06:24 -0300

    mapa

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,8 @@
 <template>
   <div>
-    <h1 v-text="titulo"></h1>
-    <ul>
-      <li v-for="(foto, index) in fotos" :key="index" >
-        <img :alt="foto.titulo" :src="foto.url">
-        </li>
-    </ul>
-    
+    <div id="map">
+      <div class="mark"></div>
+    </div>
   </div>
 </template>
 
@@ -15,23 +11,30 @@
 export default {
   data() {
     return {
-      titulo: 'Teste vsue',
-      fotos: [
-        {
-          url: 'https://hapocompany.com/wp-content/uploads/2021/10/Grupo-363.png',
-          titulo: 'HF',
-        },
-        {
-          url: 'https://hapocompany.com/wp-content/uploads/2021/10/Grupo-363.png',
-          titulo: 'HF',
-        }
-      ]
+      map:"../assets/map.jpg"
     }
-  },
-}
+  }
+  }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+#map {
+  width: 1920px;
+  height: 1920px;
+  background-image: url('../assets/map.jpg');
+  background-size: cover;
+  position: relative;
+}
 
+.mark{
+  display: inline;
+  width: 20px;
+  height: 20px;
+  background: greenyellow;
+  position: absolute;
+  top: 500px;
+  left: 500px;
+
+}
 </style>
