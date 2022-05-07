89beb432f34010bb55b5e879b3f38517fee5e41f
antonarcher <63156143+antonarcher@users.noreply.github.com>
2022-05-07 14:53:08 -0300

    mark

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,62 @@
 <template>
-  <div id="app">
-    <HelloWorld/>
+  <div>
+    <div id="map">
+      <MarcaPlayer  :x="x" :y="y"/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MarcaPlayer from './components/MarcaPlayer.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    MarcaPlayer
+  },
+  data: function(){
+    
+    return{
+        players:[],
+        x:'',
+        y:''
+    }
+
+  },
+
+  created: function(){
+      console.log("Starting connection to WebSocket Server")
+      this.connection = new WebSocket("ws://189.59.57.77:3001/geolocation")
+
+      this.connection.addEventListener("message", (message) =>{
+        this.players = JSON.parse(message.data)
+        this.x = this.convertCord(this.players[0].x)
+        this.y = this.convertCord(this.players[0].y)
+        
+      });
+
+      this.connection.onopen = function(event) {
+        console.log(event)
+        console.log("Successfully connected to the echo websocket server...")
+      }
+    },
+    methods:{
+      convertCord(entrada){
+      return String(entrada / 15) + "%"
 }
+    }
+}
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#map {
+  width: 1750px;
+  height: 1750px;
+  background-image: url('/src/assets/map.jpg');
+  background-size: 4100px 4100px;
+  background-position: center;
+  position: relative;
 }
 </style>
---
+++ src/components/MarcaPlayer.vue
@@ -0,0 +1,51 @@
+<template>
+      <div  v-bind:style="{ bottom: this.y, left: this.x }" class="mark">{{x}}</div>
+</template>
+
+<script>
+
+
+export default {
+
+    props:['x','y'],
+
+    data(){
+      return{
+        styleObject: {
+          top:this.x,
+          left:this.y
+        }
+      }
+
+    },
+    computed:{
+      atualizaX(){
+        
+        return  this.x
+      },
+      atualizaY(){
+        return  this.y
+      }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+
+.mark{
+  display: inline;
+  width: 20px;
+  height: 20px;
+  background: greenyellow;
+  position: absolute;
+  
+
+}
+</style>
+
+
+
+
+
